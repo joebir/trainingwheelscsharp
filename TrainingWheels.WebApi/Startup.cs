2fcc34037a5359724fee746d6ebb0b6c35933101
David Fraley <fraleyds@gmail.com>
2018-01-05 12:58:39 -0500

    Added cors to allow angular to communicate with the API

--- TrainingWheels.WebApi/Startup.cs
+++ TrainingWheels.WebApi/Startup.cs
@@ -12,6 +12,7 @@ namespace TrainingWheels.WebApi
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
     }
